7a6347010b1ad141fea4981156e0ba3eeba825e6
Arnold Restrepo <restrepo_arnold@outlook.com>
2018-08-21 23:36:42 -0500

    Structure Home

--- index.php
+++ index.php
@@ -5,9 +5,147 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/responsive.css">
 </head>
 <body>
-    <h2></h2>
+    <div class="Wrapper">
+        <!-- Navbar Left -->
+        <nav class="Navbar">
+            <div class="Navbar__logo">
+                <img src="http://ximg.es/200x100/000/fff" alt="Logotipo">
+            </div>
+            <ul class="Navbar__list">
+                <li class="Navbar__item">
+                    <a href="#" class="Navbar__link">
+                        <i class="Navbar__link__icon">Nuestra Razón</i>
+                    </a>
+                </li>
+                <li class="Navbar__item">
+                    <a href="#" class="Navbar__link">
+                        <i class="Navbar__link__icon">Experiencia</i>
+                    </a>
+                </li>
+                <li class="Navbar__item">
+                    <a href="#" class="Navbar__link">
+                        <i class="Navbar__link__icon">Hemos Logrado</i>
+                    </a>
+                </li>
+                <li class="Navbar__item">
+                    <a href="#" class="Navbar__link">
+                        <i class="Navbar__link__icon">Desarrollo</i>
+                    </a>
+                </li>
+                <li class="Navbar__item">
+                    <a href="#" class="Navbar__link">
+                        <i class="Navbar__link__icon">Equipo</i>
+                    </a>
+                </li>
+                <li class="Navbar__item">
+                    <a href="#" class="Navbar__link">
+                        <i class="Navbar__link__icon">Aliados</i>
+                    </a>
+                </li>
+                <li class="Navbar__item">
+                    <a href="#" class="Navbar__link">
+                        <i class="Navbar__link__icon">Blog</i>
+                    </a>
+                </li>
+                <li class="Navbar__item">
+                    <a href="#" class="Navbar__link">
+                        <i class="Navbar__link__icon">Contáctenos</i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- Header -->
+        <header class="Header">
+            <!-- NavList Header -->
+            <ul class="NavList">
+                <li class="NavList__item">
+                    <a href="#" class="NavList__link">Contáctenos</a>
+                    </li>
+                <li class="NavList__item">
+                    <a href="#" class="NavList__link">Únete al equipo JA&A</a>
+                </li>
+            </ul>
+            <!-- Language -->
+            <p class="Language"><a href="#" class="Language__link">EN</a></p>
+            <!-- Search -->
+            <form action="" class="Search">
+                <input class="Search__input" type="text" name="Buscar" id="Buscar">
+                <button class="Search__icon" type="button">Buscar</button>
+            </form>
+        </header>
+        
+        <!-- Main -->
+        <main class="Main">
+            <!-- Slider -->
+                <p>Espacio para el Slider</p>
+            <!-- Slider -->
+        </main>
+
+        <!-- Footer -->
+        <footer class="Footer">
+            <!-- Widget -->
+            <div class="Widget">
+                <img src="http://ximg.es/200x100/000/fff" alt="" class="Widget__img">
+                 <ul class="Widget__list">
+                    <li class="Widget__item">
+                        <a href="#" class="Widget__link">Oficina: 131231231231</a>
+                    </li>
+                    <li class="Widget__item">
+                        <a href="#" class="Widget__link">Oficina: 131231231231</a>
+                    </li>
+                    <li class="Widget__item">
+                        <a href="#" class="Widget__link">Oficina: 131231231231</a>
+                    </li>
+                    <li class="Widget__item">
+                        <a href="#" class="Widget__link">Oficina: 131231231231</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- Widget -->
+            <div class="Widget">
+                <h3 class="Widget__title">Oficinas</h3>
+                <ul class="Widget__list">
+                    <li class="Widget__item">
+                        <a href="#" class="Widget__link">Oficina: 131231231231</a>
+                    </li>
+                    <li class="Widget__item">
+                        <a href="#" class="Widget__link">Oficina: 131231231231</a>
+                    </li>
+                    <li class="Widget__item">
+                        <a href="#" class="Widget__link">Oficina: 131231231231</a>
+                    </li>
+                    <li class="Widget__item">
+                        <a href="#" class="Widget__link">Oficina: 131231231231</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- Widget -->
+            <div class="Widget">
+                <h3 class="Widget__title">Oficinas</h3>
+                <ul class="Widget__list">
+                    <li class="Widget__item">
+                        <a href="#" class="Widget__link">Oficina: 131231231231</a>
+                    </li>
+                    <li class="Widget__item">
+                        <a href="#" class="Widget__link">Oficina: 131231231231</a>
+                    </li>
+                    <li class="Widget__item">
+                        <a href="#" class="Widget__link">Oficina: 131231231231</a>
+                    </li>
+                    <li class="Widget__item">
+                        <a href="#" class="Widget__link">Oficina: 131231231231</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+
+    </div>
 </body>
 </html>
 
